9ef10956d4ed1c0d4b85019702fb898f3dd535fa
Daniel Hough <hough.daniel.s@gmail.com>
2026-02-27 18:04:37 -0800

    Added basic game logic and some input error handling

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,41 @@ fn main() {
 
     println!("I am thinking of a number from 1 to 100...");
     let rand_num: u8 = rand::random_range(1..100);
-    //println!("SECRET: Random number = {rand_num}");
+    println!("SECRET: Random number = {rand_num}");
 
-    println!("Please input your guess: ");
+    let mut n_guess: u8 = 7;
+    while n_guess > 0 {
+        println!("You have {} guesses remaining.", n_guess);
+        println!("Please input your guess: ");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read from stdin");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("user input should not be empty");
+        // Parse input string into u8 in range 1..100
+        let guess_u8: u8 = guess.trim()
+            .parse::<u8>()
+            .expect("Input should be an integer");
+        match guess_u8 {
+            1..100 => {
+                if guess_u8 == rand_num {
+                    println!("Your guess of {} was correct!", guess_u8);
+                    break;
+                } else {
+                    println!("Your guess of {} was incorrect.", guess_u8);
+                    n_guess -= 1;
+                }
+            },
+            _ => {
+                eprintln!("Guess should be between 1 and 100 (inclusive).");
+                continue
+            }
+        }
+        
+        
+    }
     
-    println!("You guessed: {guess}");
+
+
 }
